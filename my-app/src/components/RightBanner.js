bd37024623d6ba5988a07073386b3560398e8e36
Avi Bandi <bandiavi000@isd284.com>
2024-03-31 00:01:15 -0500

    added aspect auto on img
    idk if this actualyl did anything tho

--- my-app/src/components/RightBanner.js
+++ my-app/src/components/RightBanner.js
@@ -4,7 +4,7 @@ const RightBanner = () => {
   return (
     <div className="w-full lgl:w-1/2 flex justify-center items-center relative">
       <img
-        className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10"
+        className="w-[300px] h-[400px] aspect-auto lgl:w-[500px] lgl:h-[680px] z-10"
         src={bannerImg}
         alt="bannerImg"
       />
