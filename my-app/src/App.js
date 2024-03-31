adc750d3231bdea4ea014da948268402654ded3a
Avi Bandi <bandiavi000@isd284.com>
2024-03-30 19:28:23 -0500

    added all the react stuff

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="mx-auto items-center">
+        fuck u gag
+      </h1>
+    </div>
+  );
+}
+
+export default App;
