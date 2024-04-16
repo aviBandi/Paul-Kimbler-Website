cb985cbf6c9f06cbd1ba79102024c8ddf052f609
Avi Bandi <bandiavi000@isd284.com>
2024-04-16 00:15:12 -0500

    updated the web stuff

--- my-app/src/components/RightBanner.js
+++ my-app/src/components/RightBanner.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { bannerImg } from '../assets';
 const RightBanner = () => {
   return (
-    <div className="w-full lgl:w-1/2 flex justify-center items-center relative sm:mb-10">
+    <div className="w-full lgl:w-1/2 flex justify-center items-center relative">
       <img
         className="w-[300px] h-[400px] aspect-auto lgl:w-[500px] lgl:h-[680px] z-10"
         src={bannerImg}
